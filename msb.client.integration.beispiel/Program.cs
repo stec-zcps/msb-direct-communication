41e946dd894720f8e339833bb980f0a9b17ba550
dbb <dbb@ipa.fraunhofer.de>
2020-06-23 17:30:09 +0200

    arbeit fuer direkte integration in client

--- msb.client.integration.beispiel/Program.cs
+++ msb.client.integration.beispiel/Program.cs
@@ -11,6 +11,10 @@ namespace msb.client.integration.beispiel
     using Fraunhofer.IPA.MSB.Client.Websocket.Sample.Events;
     using msb.separate.direct.tcp;
     using Serilog;
+    using System.Collections.Generic;
+
+    public static void 
+
     public class Program
     {
         public static void Main(string[] args)
@@ -24,8 +28,7 @@ namespace msb.client.integration.beispiel
 
             Log.Information("Sample application started");
 
-            const string MsbWebsocketInterfaceUrl = "ws://localhost:8085";
-
+            /* Seite A */
             const string MyMsbSmartObjectUuid = "1a17b5e3-3a6a-4e62-97b0-82cfdd1cc818";
             const string MyMsbSmartObjectName = "C# Sample SmartObject";
             const string MyMsbSmartObjectDescription = "{"+
@@ -33,64 +36,96 @@ namespace msb.client.integration.beispiel
                 "\"Type\":\"TCP\"," +
                 "\"Patterns\":{" +
                     "\"Events\":{" +
+                        "\"EventId\":\"String\"," +
                         "\"Ip\":\"String\"," +
                         "\"Port\":\"Int\"" +
                     "}," +
                     "\"Functions\":{" +
+                        "\"EventId\":\"String\"," +
                         "\"Ip\":\"String\"," +
                         "\"Port\":\"Int\"" +
                     "}" +
                 "}" +
             "}]" +
             "}";
-
             const string MyMsbSmartObjectToken = "30e47482-c140-49a9-a79f-6f2396d8e0ab";
 
-            const string MyMsbApplicationUuid = "46441dc8-c3ab-4c93-9632-d1f356afb8ca";
-            const string MyMsbApplicationName = "C# Sample Application";
-            const string MyMsbApplicationDescription = "Description of my C# sample Application";
-            const string MyMsbApplicationToken = "5b6b273b-18ff-420b-bbff-5f40288c18f9";
+            SmartObject myMsbSmartObject = new SmartObject(MyMsbSmartObjectUuid, MyMsbSmartObjectName, MyMsbSmartObjectDescription, MyMsbSmartObjectToken);
 
-            // Create a new MsbClient which allows SmartObjects and Applications to communicate with the MSB
-            var myMsbClient = new Fraunhofer.IPA.MSB.Client.Websocket.MsbClient(MsbWebsocketInterfaceUrl);
+            var tcp_pub1 = new TCPConfiguration.TCPPublicationInstruction() { EventId = "SimpleEventId", Ip = "127.0.0.1", Port = 1884 };
+            var tcp_pub2 = new TCPConfiguration.TCPPublicationInstruction() { EventId = "FlatEventId", Ip = "127.0.0.1", Port = 1884 };
+            var tcp_pub3 = new TCPConfiguration.TCPPublicationInstruction() { EventId = "ComplexEventId", Ip = "127.0.0.1", Port = 1884 };
 
-            // Create the self description of a sample SmartObject and a sample Application
-            SmartObject myMsbSmartObject = new SmartObject(MyMsbSmartObjectUuid, MyMsbSmartObjectName, MyMsbApplicationDescription, MyMsbSmartObjectToken);
-            Application myMsbApplication = new Application(MyMsbApplicationUuid, MyMsbApplicationName, MyMsbSmartObjectDescription, MyMsbApplicationToken);
+            myMsbSmartObject.AddConfigurationParameter("msb.separate.direct.tcp.publications", new ConfigurationParameterValue(new List<Object>() {tcp_pub1, tcp_pub2, tcp_pub3 }));
 
-            // Add configuration parameters
-            myMsbSmartObject.AddConfigurationParameter("sampleParameter1", new ConfigurationParameterValue(1337));
-            myMsbSmartObject.AddConfigurationParameter("sampleParameter2", new ConfigurationParameterValue("SampleValue"));
-
-            // Add events
             Event simpleEvent = new Event("SimpleEventId", "Name of simple event", "Event with simple data format", typeof(string));
             Event flatEvent = new Event("FlatEventId", "Name of flat event", "Event with flat data format", typeof(SimpleEvent));
             Event complexEvent = new Event("ComplexEventId", "Name of complex event", "Event with nested data format", typeof(ComplexEvent));
-            myMsbApplication.AddEvent(simpleEvent);
-            myMsbApplication.AddEvent(flatEvent);
-            myMsbApplication.AddEvent(complexEvent);
-
-            // Add functions
-            SampleFunctionHandler simpleFunctions = new SampleFunctionHandler();
-            myMsbSmartObject.AddFunctionHandler(simpleFunctions);
+            myMsbSmartObject.AddEvent(simpleEvent);
+            myMsbSmartObject.AddEvent(flatEvent);
+            myMsbSmartObject.AddEvent(complexEvent);
 
             var config_myMsbSmartObject = new TCPConfiguration();
             config_myMsbSmartObject.publications = new System.Collections.Generic.Dictionary<string, TCPConfiguration.TCPPublicationInstruction>();
-            config_myMsbSmartObject.publications.Add("SimpleEventId", new TCPConfiguration.TCPPublicationInstruction() { EventId = "SimpleEventId", Ip = "127.0.0.1", Port = 1884 });
-            config_myMsbSmartObject.publications.Add("FlatEventId", new TCPConfiguration.TCPPublicationInstruction() { EventId = "FlatEventId", Ip = "127.0.0.1", Port = 1884 });
-            config_myMsbSmartObject.publications.Add("ComplexEventId", new TCPConfiguration.TCPPublicationInstruction() { EventId = "ComplexEventId", Ip = "127.0.0.1", Port = 1884 });
+
+            var list_pub = (List<TCPConfiguration.TCPPublicationInstruction>)myMsbSmartObject.Configuration.Parameters["msb.separate.direct.tcp.publications"].Value;
+            foreach (var cp in list_pub)
+            {
+                config_myMsbSmartObject.publications.Add(cp.EventId, cp);
+            }
+
             var tcp_myMsbSmartObject = new TCPInterface(config_myMsbSmartObject);
             tcp_myMsbSmartObject.Start();
-            
+
+            /* Seite B */
+            const string MyMsbApplicationUuid = "46441dc8-c3ab-4c93-9632-d1f356afb8ca";
+            const string MyMsbApplicationName = "C# Sample Application";
+            const string MyMsbApplicationDescription = "{" +
+            "\"Separate\":[{" +
+                "\"Type\":\"TCP\"," +
+                "\"Patterns\":{" +
+                    "\"Events\":{" +
+                        "\"EventId\":\"String\"," +
+                        "\"Ip\":\"String\"," +
+                        "\"Port\":\"Int\"" +
+                    "}," +
+                    "\"Functions\":{" +
+                        "\"EventId\":\"String\"," +
+                        "\"Ip\":\"String\"," +
+                        "\"Port\":\"Int\"" +
+                    "}" +
+                "}" +
+            "}]" +
+            "}";
+            const string MyMsbApplicationToken = "5b6b273b-18ff-420b-bbff-5f40288c18f9";
+
+            Application myMsbApplication = new Application(MyMsbApplicationUuid, MyMsbApplicationName, MyMsbApplicationDescription, MyMsbApplicationToken);
+
+            SampleFunctionHandler simpleFunctions = new SampleFunctionHandler();
+            myMsbSmartObject.AddFunctionHandler(simpleFunctions);
+
+            var tcp_sub1 = new TCPConfiguration.TCPSubscriptionInstruction() { EventId = "SimpleEventId", Ip = "127.0.0.1", Port = 1884, FunctionPointer = null, IntegrationFlow = new System.Collections.Generic.Dictionary<string, string>() { { "SampleFunctionWithParameters", "TestString" } } };
+
+            myMsbApplication.AddConfigurationParameter("msb.separate.direct.tcp.subscriptions", new ConfigurationParameterValue(tcp_sub1));
+
             var config_myMsbApplication = new TCPConfiguration();
             config_myMsbApplication.subscriptions = new System.Collections.Generic.Dictionary<string, TCPConfiguration.TCPSubscriptionInstruction>();
             var fPtr = msb.separate.Interfaces.BaseInterfaceUtils.CreateFunctionPointer(typeof(SampleFunctionHandler).GetMethod("EmptySampleFunction"), null);
-            var intFlow = new System.Collections.Generic.Dictionary<string, string>() { { "SampleFunctionWithParameters", "TestString" } };
-            config_myMsbApplication.subscriptions.Add("SimpleEventId", new TCPConfiguration.TCPSubscriptionInstruction() { EventId = "SimpleEventId", Ip = "127.0.0.1", Port = 1884, FunctionPointer = fPtr, IntegrationFlow = intFlow });
+
+            var list_sub = (List<TCPConfiguration.TCPSubscriptionInstruction>)myMsbSmartObject.Configuration.Parameters["msb.separate.direct.tcp.subscriptions"].Value;
+            foreach (var cp in list_sub)
+            {
+                cp.FunctionPointer = fPtr;
+                config_myMsbApplication.subscriptions.Add(cp.EventId, cp);
+            }
+
             var tcp_myMsbApplication = new TCPInterface(config_myMsbApplication);
             tcp_myMsbApplication.Start();
 
-            // Connect to the MSB and register the sample SmartObject and sample Application via the MsbClient
+            //starten
+            const string MsbWebsocketInterfaceUrl = "ws://localhost:8085";
+            var myMsbClient = new Fraunhofer.IPA.MSB.Client.Websocket.MsbClient(MsbWebsocketInterfaceUrl);
+
             myMsbClient.ConnectAsync().Wait();
 
             myMsbClient.RegisterAsync(myMsbSmartObject).Wait();
